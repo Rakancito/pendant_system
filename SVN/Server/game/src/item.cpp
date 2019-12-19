607c57f6b3de1dd43ded70157330b7ccf0383aa7
Robert <55710848+Rakancito@users.noreply.github.com>
2019-12-18 20:24:45 -0600

    Update item.cpp

--- SVN/Server/game/src/item.cpp
+++ SVN/Server/game/src/item.cpp
@@ -3,10 +3,30 @@
 	else if (GetWearFlag() & WEARABLE_ARROW)
 		return WEAR_ARROW;
 
+// or 
+
+	else if (GetWearFlag() & WEARABLE_EAR)
+		return WEAR_EAR;
+
 // Add after
 
 #ifdef ITEM_TALISMAN_EQUIPMENT
 	else if (GetWearFlag() & WEARABLE_PENDANT)
+		return WEAR_PENDANT;
+#endif
+
+// okey, it's important this part, if you not find WEARABLE because you are using a different version of src you need search, and thanks
+// .Rewn for the fix.
+
+// Search
+
+	else if (GetSubType() == ARMOR_EAR)
+		return WEAR_EAR;
+
+// Add after
+
+#ifdef ITEM_TALISMAN_EQUIPMENT
+	else if (GetWearFlag() & ARMOR_PENDANT)
 		return WEAR_PENDANT;
 #endif
 
